eb948e50394cd38f51e3649a55f18c8e353d8a15
AllJavaStuff <nikunj.patel080690@gmail.com>
2016-02-24 16:18:25 +0530

    Added files via upload

--- day2day/src/io/ConsoleReader.java
+++ day2day/src/io/ConsoleReader.java
@@ -1,4 +1,5 @@
 package io;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,30 +9,38 @@ public class ConsoleReader {
 
 	public static void main(String[] args) {
 
-		
-		//1. Using Buffered Reader
-		
-		BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
-		String l ;
-		try {
-			while ((l=a.readLine())!=null){
-				System.out.println(l);
-				
-				
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
-		//2. using Scanner
+		// 1. Using Buffered Reader
+
+		readUsingBufferedReader();
+
+		// 2. using Scanner
+
+		readUsingScanner();
+
+	}
+
+	private static void readUsingScanner() {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		String line;
 		while ((line = sc.nextLine()) != null) {
 			System.out.println(line);
 		}
 
+	}
+
+	private static void readUsingBufferedReader() {
+		// TODO Auto-generated method stub
+		BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
+		String l;
+		try {
+			while ((l = a.readLine()) != null) {
+				System.out.println(l);
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
